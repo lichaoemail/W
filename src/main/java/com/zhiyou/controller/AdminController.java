b0e3409248186436544b126ed34ae1f7da5c266d
li_chao_email <li_chao_email@163.com>
2019-12-02 21:43:43 +0800

    1212

--- src/main/java/com/zhiyou/controller/AdminController.java
+++ src/main/java/com/zhiyou/controller/AdminController.java
@@ -2,45 +2,56 @@ package com.zhiyou.controller;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.zhiyou.model.Admin;
 import com.zhiyou.model.User;
 import com.zhiyou.service.UserService;
+//import com.zhiyou100.entity.ResponseVo;
+//import com.zhiyou100.entity.UserLoginException;
 
 @Controller
+@RequestMapping("login")
 public class AdminController {
+//	@Autowired
+//	private UserService service;
+//	
+//	@PostMapping
+//	public @ResponseBody ResponseVo<User> login(@RequestBody User u,HttpSession session)throws  UserLoginException{
+//		System.out.println("登陆：：u="+u);
+//		User dbUser=service.login(u.getUname(), u.getUpwd());
+//		session.setAttribute("user", dbUser);
+//		return new ResponseVo<User>(dbUser, 200, "登陆成功！");
+//	}
+//	@PutMapping("/{uid}")
+//	public @ResponseBody ResponseVo<String> logout(@PathVariable("uid") Integer uid,HttpSession session)throws  UserLoginException{
+//		System.out.println("登出：：uid="+uid);
+//		service.logOut(uid);
+//		session.invalidate();
+//		return new ResponseVo<String>("", 200, "登出成功！");
+//	}
+	
 	@Autowired
-	UserService service;	
-	@RequestMapping("show")
-	public String show(HttpServletRequest req,HttpServletResponse rep) {
-		req.setAttribute("list",service.selectAll());
-		return "show";
-	}
+	private UserService service;
 	
-	@RequestMapping("add")
-	public String add(User user,HttpServletRequest req,HttpServletResponse rep) {
-		service.add(user);
-		return "redirect:show";
-	}
+
+	 @RequestMapping(value = "islogin", method = RequestMethod.POST)
+	 public String isLogin(Admin admin) {
+		System.out.println("accounts=" + admin.getAccounts()+",password="+ admin.getPassword());
+		return "sendRedirect:success";
+	 }	
+
 	
-	@RequestMapping("delete")
-	public String add(int id,HttpServletRequest req,HttpServletResponse rep) {
-		service.delete(id);
-		return "redirect:show";
-	}
 	
-	@RequestMapping("selectByID")
-	public String selectByID(int id,HttpServletRequest req,HttpServletResponse rep) {
-		req.setAttribute("user", service.selectByID(id));
-		return "update";
-	}
-	
-	@RequestMapping("update")
-	public String update(User user,HttpServletRequest req,HttpServletResponse rep) {
-		service.update(user);
-		return "redirect:show";
-	}
+
 }
--- src/main/java/com/zhiyou/model/Admin.java
+++ src/main/java/com/zhiyou/model/Admin.java
@@ -12,4 +12,44 @@ public class Admin {
 	private String accounts;
 	private String password;
 	private String admin_remark;
+	public Integer getAdmin_id() {
+		return admin_id;
+	}
+	public void setAdmin_id(Integer admin_id) {
+		this.admin_id = admin_id;
+	}
+	public String getAccounts() {
+		return accounts;
+	}
+	public void setAccounts(String accounts) {
+		this.accounts = accounts;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getAdmin_remark() {
+		return admin_remark;
+	}
+	public void setAdmin_remark(String admin_remark) {
+		this.admin_remark = admin_remark;
+	}
+	public Admin(Integer admin_id, String accounts, String password, String admin_remark) {
+		super();
+		this.admin_id = admin_id;
+		this.accounts = accounts;
+		this.password = password;
+		this.admin_remark = admin_remark;
+	}
+	public Admin() {
+		super();
+	}
+	@Override
+	public String toString() {
+		return "Admin [admin_id=" + admin_id + ", accounts=" + accounts + ", password=" + password + ", admin_remark="
+				+ admin_remark + "]";
+	}
+	
 }
